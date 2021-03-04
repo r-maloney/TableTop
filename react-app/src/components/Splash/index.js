664d3b634dc2f6c912c28c98bba67eff033b03e7
Ryan <rdmaloney3@gmail.com>
2021-03-04 12:14:53 -0500

    carousel images cropped

--- react-app/src/components/Splash/index.js
+++ react-app/src/components/Splash/index.js
@@ -3,27 +3,36 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "./splash.css";
 
 const Splash = () => {
+  const style = {
+    backgroundImage: `url(${process.env.PUBLIC_URL}/images/health-carousel-cropped.png)`,
+  };
+
   return (
-    <Carousel
-      showThumbs={false}
-      infiniteLoop
-      autoPlay
-      stopOnHover={true}
-      interval='5000'
-    >
-      <div>
-        <img src='/images/school-carousel.jpg' alt='School carousel'></img>
-      </div>
-      <div>
-        <img src='/images/health-carousel.jpg' alt='School carousel'></img>
-      </div>
-      <div>
-        <div>
-          <img src='/images/food-carousel.png' alt='School carousel'></img>
-          <p>Hello</p>
-        </div>
-      </div>
-    </Carousel>
+    <></>
+
+    // <Carousel
+    //   showThumbs={false}
+    //   infiniteLoop
+    //   autoPlay
+    //   stopOnHover={true}
+    //   interval='5000'
+    // >
+    //   <div className='slide1' style={style}>
+    //     <img src='/images/school-carousel.jpg' alt='School carousel'></img>
+    //   </div>
+    //   <div className='slide1'>
+    //     <img src='/images/health-carousel.jpg' alt='School carousel'></img>
+    //   </div>
+    //   <div>
+    //     <div>
+    //       <img
+    //         src='/images/food-carousel-cropped.png'
+    //         alt='School carousel'
+    //       ></img>
+    //       <p>Hello</p>
+    //     </div>
+    //   </div>
+    // </Carousel>
   );
 };
 
