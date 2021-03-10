b79c3ce45108ad3e4b7565a4eb2f05e11c3e4d63
Ryan <rdmaloney3@gmail.com>
2021-03-10 10:22:27 -0700

    switched to wave orange background

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -4,21 +4,34 @@ import LogoutButton from "../auth/LogoutButton";
 import logo from "../../images/logos/TTLogo.png";
 import "./navigation.css";
 
-const Navigation = ({ setAuthenticated }) => {
+const Navigation = ({ authenticated, setAuthenticated }) => {
   return (
     <div className='nav__container'>
       <div className='nav__logo-container'>
         <img className='nav__logo' src={logo} alt='TT logo'></img>
         <h2>TABLETOP</h2>
       </div>
-      <div className='nav__space'></div>
+      <div className='nav__browse'>
+        {authenticated && (
+          <ul>
+            <li>
+              <NavLink to='/explore' exact={true} activeClassName='active'>
+                Explore
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to='/give' exact={true} activeClassName='active'>
+                Give
+              </NavLink>
+            </li>
+            <NavLink to='/cart' exact={true} activeClassName='active'>
+              Shopping Cart
+            </NavLink>
+          </ul>
+        )}
+      </div>
       <div className='nav__links'>
         <ul>
-          <li>
-            <NavLink to='/' exact={true} activeClassName='active'>
-              Home
-            </NavLink>
-          </li>
           <li>
             <NavLink to='/login' exact={true} activeClassName='active'>
               Login
--- react-app/src/components/Splash/index.js
+++ react-app/src/components/Splash/index.js
@@ -31,6 +31,17 @@ const Splash = () => {
             {registration && <SignUpForm />}
           </div>
         </div>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          viewBox='0 0 1440 319'
+          className='splash__wave'
+        >
+          <path
+            fill='#FFF'
+            fill-opacity='1'
+            d='M0,64L60,69.3C120,75,240,85,360,117.3C480,149,600,203,720,213.3C840,224,960,192,1080,181.3C1200,171,1320,181,1380,186.7L1440,192L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z'
+          ></path>
+        </svg>
       </div>
       <div className='splash__steps'>
         <div className='splash__step'>
