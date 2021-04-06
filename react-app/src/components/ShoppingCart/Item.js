f834fac0924cbf525f9b4da500a26fd28522d780
Ryan <rdmaloney3@gmail.com>
2021-04-05 22:39:41 -0400

    moved updated quantity functions to items component

--- react-app/src/components/ShoppingCart/Item.js
+++ react-app/src/components/ShoppingCart/Item.js
@@ -1,14 +1,34 @@
-const Item = ({ item, decreaseCount, increaseCount }) => {
+import { useDispatch } from "react-redux";
+import { useState } from "react";
+import { addToCart } from "../../store/cart";
+
+const Item = ({ item, cart }) => {
+  const dispatch = useDispatch();
+  const [quantity, setQuantity] = useState(item.quantity);
+
+  const increaseCount = async (item) => {
+    const newItem = await dispatch(addToCart(item, cart.id));
+
+    // setCount(newCount);
+    // let newId = parseInt(id);
+    // let cart = user.cart;
+    // console.log(cart[1], cart[newId], cart[id], cart.id, cart.newId);
+    // console.log(user.cart, user.cart[id]);
+    // let cart = user.cart[item_id]["count"]++;
+    // dispatch(userCart(user, cart));
+  };
+  const decreaseCount = (item_id) => {
+    // let cart = user.cart[item_id];
+    // dispatch(userCart(user, cart));
+  };
+
   return (
     <div key={item.id} className='cart__item'>
-      <div className='cart__name'>
-        {item.name}
-        {item.id}
-      </div>
+      <div className='cart__name'>{item.name}</div>
       <button className='cart__minus' onClick={() => decreaseCount(item)}>
         -
       </button>
-      <div className='cart__count'>{item.quantity}</div>
+      <div className='cart__count'>{quantity}</div>
       <button className='cart__plus' onClick={() => increaseCount(item)}>
         +
       </button>
--- react-app/src/components/ShoppingCart/ShoppingCart.js
+++ react-app/src/components/ShoppingCart/ShoppingCart.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import "../Explore/explore.css";
-import { addToCart, getCart } from "../../store/cart";
+import { getCart } from "../../store/cart";
 import Item from "./Item";
 
 const ShoppingCart = () => {
@@ -21,13 +21,10 @@ const ShoppingCart = () => {
     } else {
       item["quantity"] = 1;
       cartCount[item.id] = item;
-      console.log(item);
     }
   }
 
   const cartSet = Object.values(cartCount);
-
-  //Need to add item component. Get object.values from cartCount and map over array. Passing in item as props
 
   useEffect(() => {
     if (user) dispatch(getCart(user));
@@ -47,40 +44,11 @@ const ShoppingCart = () => {
     history.push("/thank-you");
   };
 
-  const calculateCount = (id) => {
-    cartArr.filter((item) => (item.id = id));
-    return cartArr.length;
-  };
-
-  const increaseCount = async (item) => {
-    await dispatch(addToCart(item, cart.id));
-    console.log("dispatched addtocart");
-    // const newCount = count + 1;
-    // setCount(newCount);
-    // let newId = parseInt(id);
-    // let cart = user.cart;
-    // console.log(cart[1], cart[newId], cart[id], cart.id, cart.newId);
-    // console.log(user.cart, user.cart[id]);
-    // let cart = user.cart[item_id]["count"]++;
-    // dispatch(userCart(user, cart));
-  };
-  const decreaseCount = (item_id) => {
-    // let cart = user.cart[item_id];
-    // dispatch(userCart(user, cart));
-  };
-
   return (
     <div className='cart__container'>
       <h1>Shopping Cart</h1>
       <div>
-        {loaded &&
-          cartSet.map((item) => (
-            <Item
-              item={item}
-              decreaseCount={decreaseCount}
-              increaseCount={increaseCount}
-            />
-          ))}
+        {loaded && cartSet.map((item) => <Item item={item} cart={cart} />)}
       </div>
       <button className='cart__order' onClick={handleSubmit}>
         Order Now
