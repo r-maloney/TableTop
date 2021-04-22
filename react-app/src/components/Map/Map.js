f317a30780342a2f3a5177d053f65c8fa0340b8c
Ryan <rdmaloney3@gmail.com>
2021-04-22 17:22:02 -0400

    missing lat/long on business model fixed bug

--- react-app/src/components/Map/Map.js
+++ react-app/src/components/Map/Map.js
@@ -17,44 +17,47 @@ const Map = () => {
   const [selectedBusiness, setSelectedBusiness] = useState(null);
 
   return (
-    <ReactMapGL
-      {...viewport}
-      onViewportChange={(newView) => setViewport(newView)}
-      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_API_KEY}
-      mapStyle={"mapbox://styles/r-maloney/ckns1ra7k010p17qvzie84vad"}
-    >
-      {businesses[0] &&
-        businesses.map((business) => (
-          <Marker
-            latitude={business.lat}
-            longitude={business.long}
-            offset={[0, -50 / 2]}
-          >
-            <button
-              className='mapbox__spot-icon'
-              onClick={(e) => {
-                e.preventDefault();
-                setSelectedBusiness(business);
-              }}
-            >
-              <span>
-                <i className='fas fa-map-pin'></i>
-              </span>
-            </button>
-          </Marker>
-        ))}
-      {selectedBusiness && (
-        <Popup
-          longitude={selectedBusiness.long}
-          latitude={selectedBusiness.lat}
-          onClose={() => setSelectedBusiness(null)}
+    <>
+      {businesses[0] && (
+        <ReactMapGL
+          {...viewport}
+          onViewportChange={(newView) => setViewport(newView)}
+          mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_API_KEY}
+          mapStyle={"mapbox://styles/r-maloney/ckns1ra7k010p17qvzie84vad"}
         >
-          <div>{selectedBusiness.name}</div>
-          <div>{selectedBusiness.description}</div>
-          {/* <img src={selectedBusiness.img_url} alt='business logo' /> */}
-        </Popup>
+          {businesses.map((business) => (
+            <Marker
+              latitude={business.lat}
+              longitude={business.long}
+              offset={[0, -50 / 2]}
+            >
+              <button
+                className='mapbox__spot-icon'
+                onClick={(e) => {
+                  e.preventDefault();
+                  setSelectedBusiness(business);
+                }}
+              >
+                <span>
+                  <i className='fas fa-map-pin'></i>
+                </span>
+              </button>
+            </Marker>
+          ))}
+          {selectedBusiness && (
+            <Popup
+              longitude={selectedBusiness.long}
+              latitude={selectedBusiness.lat}
+              onClose={() => setSelectedBusiness(null)}
+            >
+              <div>{selectedBusiness.name}</div>
+              <div>{selectedBusiness.description}</div>
+              {/* <img src={selectedBusiness.img_url} alt='business logo' /> */}
+            </Popup>
+          )}
+        </ReactMapGL>
       )}
-    </ReactMapGL>
+    </>
   );
 };
 
